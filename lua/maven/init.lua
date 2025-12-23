5577065e674d31cbd360a537e659a06a9c981db1
dzerjinsky <dzerjinsky@dev.local>
2025-12-24 00:05:18 +0700

    checkpoint

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -1,4 +1,5 @@
 local M = {}
+M.buffer = nil -- todo clear on exit
 
 local winsize = function()
     local width = vim.api.nvim_get_option('columns')
@@ -13,13 +14,17 @@ local winsize = function()
     return { wwidth, wheight, math.ceil((width - wwidth) / 2), math.ceil((height - wheight) / 2) }
 end
 
-local getBuffer = function(data)
-    local buffer = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
-    return buffer
+local init_buffer = function()
+    if M.buffer then return end
+    M.buffer = vim.api.nvim_create_buf(false, true)
 end
 
-local makeOpt = function()
+local push_buffer = function(data)
+    init_buffer()
+    vim.api.nvim_buf_set_lines(M.buffer, -1, -1, false, data)
+end
+
+local make_win_opt = function()
     local wsize = winsize()
     local opt = {
         relative = 'win',
@@ -28,37 +33,32 @@ local makeOpt = function()
         col = wsize[3],
         row = wsize[4],
         border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
-        title = 'maven clean compile',
+        title = 'maven clean compile', --support parameter 
         title_pos = 'center',
-        footer = 'press <Enter> to continue with editor',
+        footer = 'press <Enter> to continue with editor', --todo support pressing Enter
         footer_pos = 'center'
     }
     return opt
 end
 
-local getWin = function(buffer)
-    local opt = makeOpt()
-    local win = vim.api.nvim_open_win(buffer, true, opt)
-    if win == 0 then
-        vim.notify("create window error in getWin()", vim.log.levels.ERROR, {})
-    end
-    return win
+local open_win = function()
+    vim.api.nvim_open_win(M.buffer, true, make_win_opt())
 end
 
 local receiver = function(job_id, data, event)
     if data then
-        local lbuf = getBuffer(data)
-        local lwin = getWin(lbuf)
+        push_buffer(data)
+        open_win()
     end
 end
 
 M.do_mvn_clean_compile = function()
-    local cmd = { 'mvn', 'clean', 'compile' }
+    local cmd = { 'mvn', 'clean', 'compile' } --todo make parameter
     vim.fn.jobstart(cmd, {
-        stdout_buffered = true,
-        stderr_buffered = true,
-        on_stdout = receiver,
+        stdout_buffered = false,
+        on_stdout = receiver, --tode add error
     })
 end
-
+--todo make user command
+--todo make nmap for user command
 return M
